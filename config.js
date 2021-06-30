5ea1a97dc5622ffffdf4bea00f169375aad3ff89
CongDuy2307 <54467261+CongDuy2307@users.noreply.github.com>
2021-06-30 22:26:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     sinh nhật mi 
     nên tao làm cái ni
     dể tặng mi áaa`,
-    btnIntro: 'Hôm ni sinh nhật mi phải hong=))',
-    title: 'T biết rồi ',
-    desc: 'hỏi chơi á 😘 ',
-    btnYes: 'Duungs rồi áaaaa',
-    btnNo: 'Hông phải nha',
+    btnIntro: 'oceeeeee',
+    title: 'Hôm ni là sinh nhật mi thật hông á ',
+    desc: 't biết ròi hỏi chơi á 😘 ',
+    btnYes: 'Đúng rồi nhaaaa',
+    btnNo: 'Hông phải nha=)))',
     question:'T có lời chúc mi nàyyy',
-    btnReply: 'nhập bất kì cái gì đi',
+    btnReply: 'Iuuuuu',
     reply: 'Thêm một tuổi ròi chúc bé sinh nhật vv và học giỏi hơn nhaa',
     mess: 'bữa ni hong đi chơi dc ròii',
-    messDesc: 'CÓ gì vô năm bù sau nháaaaa.',
+    messDesc: 'Có gì vô năm bù sau nháaaaa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
