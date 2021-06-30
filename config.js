bd90d710aeb1cffd4ccdb76d9f9b506f8277eb26
CongDuy2307 <54467261+CongDuy2307@users.noreply.github.com>
2021-06-30 22:17:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Quỳnh ơi!',
+    introDesc: `Hôm ni là 
+    sinh nhật mi 
+    nên tao làm cái ni
+    dể tặng mi áaa`,
+    btnIntro: 'Hôm ni sinh nhật mi phải hong=))',
+    title: 'T biết rồi ',
+    desc: 'hỏi chơi á 😘 ',
+    btnYes: 'Duungs rồi áaaaa',
+    btnNo: 'Hông phải nha',
+    question:'T có lời chúc mi nàyyy',
+    btnReply: 'nhập bất kì cái gì đi',
+    reply: 'Thêm một tuổi ròi chúc bé sinh nhật vv và học giỏi hơn nhaa',
+    mess: 'bữa ni hong đi chơi dc ròii',
+    messDesc: 'CÓ gì vô năm bù sau nháaaaa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
